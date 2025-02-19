16314fcec727f607918f1d30a45e067a667afa87
yeoniii20 <commetoi_yeoni@naver.com>
2025-02-19 21:13:06 +0900

    feat: 토큰 타입 및 유틸 추가

---
+++ app/types/tokenType.ts
@@ -0,0 +1,9 @@
+export type TokenType =
+  | "C_TOKEN_1"
+  | "C_TOKEN_2"
+  | "C_TOKEN_3"
+  | "C_TOKEN_4"
+  | "C_PRIMARY"
+  | "C_SUB2"
+  | "C_TOKEN_6"
+  | "C_TOKEN_7";
---
+++ app/utils/badgeColor.ts
@@ -0,0 +1,74 @@
+import { TokenType } from "../types/tokenType";
+
+export const tokenBg: TokenType[] = [
+  "C_TOKEN_1",
+  "C_TOKEN_2",
+  "C_TOKEN_3",
+  "C_TOKEN_4",
+  "C_PRIMARY",
+  "C_SUB2",
+  "C_TOKEN_6",
+  "C_TOKEN_7",
+];
+
+export const tokenColorMapping = {
+  C_TOKEN_1: {
+    bg: "bg-token_1_bg",
+    text: "text-token_1",
+  },
+  C_TOKEN_2: {
+    bg: "bg-token_2_bg",
+    text: "text-token_2",
+  },
+  C_TOKEN_3: {
+    bg: "bg-token_3_bg",
+    text: "text-token_3",
+  },
+  C_TOKEN_4: {
+    bg: "bg-token_4_bg",
+    text: "text-token_4",
+  },
+  C_PRIMARY: {
+    bg: "bg-selectedoption_hover",
+    text: "text-primary",
+  },
+  C_SUB2: {
+    bg: "bg-sub2_bg",
+    text: "text-sub2",
+  },
+  C_TOKEN_6: {
+    bg: "bg-token_6_bg",
+    text: "text-token_6",
+  },
+  C_TOKEN_7: {
+    bg: "bg-token_7_bg",
+    text: "text-token_7",
+  },
+};
+
+export const colorMapping = {
+  C_TOKEN_1: {
+    bg: "bg-token_1",
+  },
+  C_TOKEN_2: {
+    bg: "bg-token_2",
+  },
+  C_TOKEN_3: {
+    bg: "bg-token_3",
+  },
+  C_TOKEN_4: {
+    bg: "bg-token_4",
+  },
+  C_PRIMARY: {
+    bg: "bg-primary",
+  },
+  C_SUB2: {
+    bg: "bg-sub2",
+  },
+  C_TOKEN_6: {
+    bg: "bg-token_6",
+  },
+  C_TOKEN_7: {
+    bg: "bg-token_7",
+  },
+};

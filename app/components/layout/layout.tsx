1204ba79301b37a5f8d20afdd813ac2f190554a0
yeoniii20 <100350818+yeoniii20@users.noreply.github.com>
2025-02-20 16:50:26 +0900

    style: 그림자 삭제

--- app/components/layout/layout.tsx
+++ app/components/layout/layout.tsx
@@ -5,7 +5,7 @@ import Footer from "./footer";
 const Layout = ({ children }: { children: ReactNode }) => {
   return (
     <div className="flex flex-col min-h-screen">
-      <header className="sticky top-0 z-50 bg-white shadow-md">
+      <header className="sticky top-0 z-50 bg-white">
         <Header isLoggedIn={false} />
       </header>
       <main className="flex-grow">{children}</main>

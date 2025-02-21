50fb65846af9b6d82c779456634da79a522e4fd5
yeoniii20 <100350818+yeoniii20@users.noreply.github.com>
2025-02-21 17:39:14 +0900

    feat: 데이터 추가

---
+++ app/data/portfolio.ts
@@ -0,0 +1,117 @@
+import apt1 from "@/app/image/test/apt1.svg";
+import apt2 from "@/app/image/test/apt2.svg";
+import apt3 from "@/app/image/test/apt3.svg";
+import apt4 from "@/app/image/test/apt4.svg";
+import apt5 from "@/app/image/test/apt5.svg";
+import apt6 from "@/app/image/test/apt6.svg";
+import apt7 from "@/app/image/test/apt7.png";
+import apt8 from "@/app/image/test/apt8.png";
+import apt9 from "@/app/image/test/apt9.png";
+import apt10 from "@/app/image/test/apt10.png";
+
+export const PORTFOLIO_DATA = [
+  {
+    title: "Title 1",
+    date: "2025-02-20",
+    description: "Description for apt1",
+    imageUrl: apt1,
+    isScrap: false,
+    id: 1,
+  },
+  {
+    title: "Title 2",
+    date: "2025-02-21",
+    description: "Description for apt2",
+    imageUrl: apt2,
+    isScrap: false,
+    id: 2,
+  },
+  {
+    title: "Title 3",
+    date: "2025-02-22",
+    description: "Description for apt3",
+    imageUrl: apt3,
+    isScrap: false,
+    id: 3,
+  },
+  {
+    title: "Title 4",
+    date: "2025-02-23",
+    description: "Description for apt4",
+    imageUrl: apt4,
+    isScrap: false,
+    id: 4,
+  },
+  {
+    title: "Title 5",
+    date: "2025-02-24",
+    description: "Description for apt5",
+    imageUrl: apt5,
+    isScrap: false,
+    id: 5,
+  },
+  {
+    title: "Title 6",
+    date: "2025-02-25",
+    description: "Description for apt6",
+    imageUrl: apt6,
+    isScrap: false,
+    id: 6,
+  },
+  {
+    title: "Title 7",
+    date: "2025-02-26",
+    description: "Description for apt7",
+    imageUrl: apt7,
+    isScrap: false,
+    id: 7,
+  },
+  {
+    title: "Title 8",
+    date: "2025-02-27",
+    description: "Description for apt8",
+    imageUrl: apt8,
+    isScrap: false,
+    id: 8,
+  },
+  {
+    title: "Title 9",
+    date: "2025-02-28",
+    description: "Description for apt9",
+    imageUrl: apt9,
+    isScrap: false,
+    id: 9,
+  },
+  {
+    title: "Title 10",
+    date: "2025-03-01",
+    description: "Description for apt10",
+    imageUrl: apt10,
+    isScrap: false,
+    id: 10,
+  },
+  {
+    title: "Title 11",
+    date: "2025-03-02",
+    description: "Description for apt11",
+    imageUrl: apt1,
+    isScrap: false,
+    id: 11,
+  },
+  {
+    title: "Title 12",
+    date: "2025-03-03",
+    description: "Description for apt12",
+    imageUrl: apt2,
+    isScrap: false,
+    id: 12,
+  },
+  {
+    title: "Title 13",
+    date: "2025-03-04",
+    description: "Description for apt13",
+    imageUrl: apt3,
+    isScrap: false,
+    id: 13,
+  },
+];

31b42e4350e111157b64f19448499508d60cba64
yeoniii20 <100350818+yeoniii20@users.noreply.github.com>
2025-02-21 17:41:30 +0900

    design: 안내문구 디자인 변경

--- app/components/alert/alert.tsx
+++ app/components/alert/alert.tsx
@@ -20,8 +20,8 @@ const Alert: React.FC<AlertProps> = ({ message, duration = 3000 }) => {
 
   return (
     <div
-      className="fixed bottom-1/2 left-1/2 transform -translate-x-1/2 py-3 px-6 rounded-md
-     text-sub2 bg-black/50 backdrop-blur-sm"
+      className="fixed z-50 bottom-1/2 left-1/2 transform -translate-x-1/2 py-2 px-3 
+      rounded-md text-sub2 bg-black/50 backdrop-blur-sm text-body3_m md:text-mobile_body3_sb"
     >
       {message}
     </div>

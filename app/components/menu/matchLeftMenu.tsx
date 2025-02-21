e0d9e55e9cae0675fd11d8e143c516860bd00622
yeoniii20 <100350818+yeoniii20@users.noreply.github.com>
2025-02-21 17:41:56 +0900

    style: 활성화 색상 변경

--- app/components/menu/matchLeftMenu.tsx
+++ app/components/menu/matchLeftMenu.tsx
@@ -12,7 +12,7 @@ const MatchLeftMenu: React.FC<LeftMenuProps> = ({
   onCategoryClick,
 }) => {
   return (
-    <div className="w-[230px] h-auto bg-white shadow-md rounded-lg p-6 flex flex-col">
+    <div className="w-[230px] h-auto p-6 pl-3 flex flex-col">
       <h2 className="font-semibold text-text_sub2 text-lg mb-4">
         공인중개사 매칭
       </h2>
@@ -21,10 +21,10 @@ const MatchLeftMenu: React.FC<LeftMenuProps> = ({
           <li key={category}>
             <button
               onClick={() => onCategoryClick(category)}
-              className={`w-full text-left text-sm py-2 px-3 rounded-lg transition-colors duration-300 ${
+              className={`w-full text-left py-2 px-3 transition-colors duration-300 ${
                 activeCategory === category
-                  ? "text-primary"
-                  : "text-text_sub hover:text-selectedoption_border"
+                  ? "text-primary border-l-2 border-primary bg-selectedoption_default text-body2_sb"
+                  : "text-text_sub text-body2_r hover:border-l-2 hover:border-primary hover:bg-token_bg hover:text-text"
               }`}
             >
               {category}

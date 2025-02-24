ed2594b6cfa5bf91a3369b92c7ae1c0d3c5c62ff
yeoniii20 <100350818+yeoniii20@users.noreply.github.com>
2025-02-24 15:49:00 +0900

    design: 반응형 디자인 추가

--- app/components/card/shortcutCard.tsx
+++ app/components/card/shortcutCard.tsx
@@ -8,14 +8,14 @@ interface ShortcutProps {
 const Shortcut = ({ color, title, description, onClick }: ShortcutProps) => {
   return (
     <div
-      className={`flex w-full pl-9 py-8 items-start flex-col gap-1.5 rounded-[10px]
+      className={`flex w-full pl-6 py-6 md:pl-9 md:py-8 items-start flex-col gap-1.5 rounded-[10px]
         text-white cursor-pointer ${
           color === "blue" ? `bg-primary` : `bg-black`
         }`}
       onClick={onClick}
     >
-      <h3 className="text-[20px] font-bold">{title}</h3>
-      <p className="text-[14px] font-normal">{description}</p>
+      <h3 className="text-mobile_h2 md:text-h2">{title}</h3>
+      <p className="text-mobile_body2_m md:text-body2_m">{description}</p>
     </div>
   );
 };

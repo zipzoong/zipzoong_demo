9147b2b4e122baab863378f0a335e2a0496cee6e
yeoniii20 <100350818+yeoniii20@users.noreply.github.com>
2025-02-24 15:50:31 +0900

    design: 반응형 디자인 추가

--- app/components/category/categoryItem.tsx
+++ app/components/category/categoryItem.tsx
@@ -8,11 +8,19 @@ interface CategoryItemProps {
 
 const CategoryItem: React.FC<CategoryItemProps> = ({ icon, label }) => {
   return (
-    <div className="flex flex-col items-center gap-3.5 py-[15px] px-[19px]">
-      <div className="p-[35px] flex items-center justify-center bg-sub2 rounded-[40px]">
-        <Image src={icon} alt={label} width={48} height={48} />
+    <div className="flex flex-col items-center gap-2 md:gap-3 py-2 px-2 md:py-6 md:px-6">
+      <div className="flex items-center justify-center bg-sub2 rounded-[40px] w-[60px] h-[60px] md:w-[118px] md:h-[118px]">
+        <Image
+          src={icon}
+          alt={label}
+          width={32}
+          height={32}
+          className="w-[32px] h-[32px] md:w-[48px] md:h-[48px]"
+        />
       </div>
-      <p className="text-[15px] font-medium text-text">{label}</p>
+      <p className="text-[12px] md:text-[14px] lg:text-[15px] font-medium text-text">
+        {label}
+      </p>
     </div>
   );
 };

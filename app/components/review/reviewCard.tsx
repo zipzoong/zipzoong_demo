fb106c9b0a62add67a32e9b3a6d580e27bfa2490
yeoniii20 <100350818+yeoniii20@users.noreply.github.com>
2025-02-24 13:17:27 +0900

    design: 반응형 디

--- app/components/review/reviewCard.tsx
+++ app/components/review/reviewCard.tsx
@@ -25,20 +25,19 @@ const ReviewCard = ({
 
   return (
     <div
-      //   className="flex items-start px-4 pt-0 pb-6 border-b-2 border-border gap-5 cursor-pointer"
-      className="flex flex-row w-full gap-2.5 bg-white border rounded-lg p-6 mx-auto
-       shadow-md hover:shadow-md cursor-pointer transition-shadow duration-300 mb-1 border-border"
+      className="flex flex-row w-full gap-2.5 bg-white border rounded-lg p-3 md:p-6 max-w-lg mx-auto
+    shadow-sm hover:shadow-md cursor-pointer transition-shadow duration-300 mb-1 border-border"
     >
-      <div className="flex w-full flex-col gap-3">
-        <div className="flex flex-row items-center gap-3.5">
+      <div className="flex w-full flex-col md:gap-3 gap-2">
+        <div className="flex flex-row items-center gap-2 md:gap-3.5">
           {/* 프로필 이미지 */}
-          <div className="mb-0 min-w-[48px] min-h-[48px]">
+          <div className="mb-0 md:min-w-[48px] md:min-h-[48px] min-w-[36px] min-h-[36px]">
             <Image
               src={imageUrl}
               alt={name}
-              width={48}
-              height={48}
-              className="w-12 h-12 rounded-full object-cover"
+              width={36}
+              height={36}
+              className="md:w-12 md:h-12 h-[36px] w-[36px] rounded-full object-cover"
             />
           </div>
           <div className="flex flex-row w-full justify-between">
@@ -46,9 +45,11 @@ const ReviewCard = ({
             <div className="flex flex-col gap-1">
               <div className="flex flex-row items-center gap-1.5">
                 <p className="text-body1_m font-semibold text-text">{name}</p>
-                <p className="text-body4_r text-text_sub3">고객님의 리뷰</p>
+                <p className="text-[10px] md:text-body4_r text-text_sub3">
+                  고객님의 리뷰
+                </p>
               </div>
-              <div className="flex gap-1">
+              <div className="flex gap-1 md:text-body3_r text-mobile_body4_r">
                 {stars.map((isFilled, index) =>
                   isFilled ? (
                     <FaStar key={index} className="text-star" />
@@ -64,8 +65,10 @@ const ReviewCard = ({
 
         {/* Title and Description */}
         <div className="flex flex-col mt-6">
-          <h3 className="text-h3 text-text line-clamp-2">{title}</h3>
-          <p className="text-body2_r text-text_sub4 mt-1 line-clamp-2">
+          <h3 className="text-mobile_h3 md:text-h3 text-text line-clamp-2">
+            {title}
+          </h3>
+          <p className="text-mobile_body3_r md:text-body2_r text-text_sub4 mt-1 line-clamp-2">
             {description}
           </p>
         </div>

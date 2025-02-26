e9198abd6971469226b895314a9b68a83eaa3e35
yeoniii20 <100350818+yeoniii20@users.noreply.github.com>
2025-02-26 09:57:26 +0900

    feat: API url 추가

--- app/api/apiUrl.ts
+++ app/api/apiUrl.ts
@@ -1,5 +1,15 @@
 export const BASE_URL = "/api/v1";
 
+// 카카오 로그인
+export const KAKAO_LOGIN = "/api/auth/kakao";
+export const LOGOUT = "/api/auth/logout";
+export const WHITDRAW = "/api/auth/withdraw";
+
+// 이메일 회원가입/로그인
+export const LOGIN = "api/v1/email/login";
+export const SING_UP_AGENT = "/api/v1/email/agent";
+export const SING_UP_GENERAL = "/api/v1/email/general";
+
 // 메인 페이지
 export const MAIN = "/api/v1/main";
 export const MAIN_MATCHING = "/api/v1/main/matching";
@@ -7,5 +17,9 @@ export const MAIN_PORTFOLIO = "/api/v1/main/portfolio";
 export const MAIN_PROFILE_DETAIL = "/api/v1/main/profile/detail";
 
 // 일반회원 마이페이지
+export const MYPAGE_GENERAL = "/api/v1/mypage/general";
 
 // 공인중개사 마이페이지
+export const MYPAGE_AGENT = "/api/v1/mypage/agent";
+export const MYPAGE_DETAIL_AGENT = "/api/v1/mypage/agent/detail";
+export const MYPAGE_PORTFOLIO_AGENT = "/api/v1/mypage/agent/portfolio";

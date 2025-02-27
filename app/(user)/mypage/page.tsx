cf0af0ccd935fe5024cbc9ee64a20934e0f6cca0
yeoniii20 <commetoi_yeoni@naver.com>
2025-02-27 22:19:51 +0900

    design: 백그라운드 변경

--- app/(user)/mypage/page.tsx
+++ app/(user)/mypage/page.tsx
@@ -4,7 +4,7 @@ import User from "@/app/components/user/user";
 
 const MyPage = () => {
   return (
-    <div className="w-full flex justify-center items-center bg-bg_sub pt-8 pb-16 md:pt-12 md:pb-24">
+    <div className="w-full bg-bg_sub">
       <div className="max-w-screen-sm sm:max-w-screen-md md:max-w-screen-lg lg:max-w-screen-lx px-4 md:px-5">
         <User />
       </div>
